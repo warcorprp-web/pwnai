c677b478d59e8b6c7c75a4c29015d1c9d4477c16
Pwnai <warcorprp@icloud.com>
2026-01-25 09:46:58 +0000

    Add WaveAI telemetry constants and MakeTEvent function

--- pkg/telemetry/telemetrydata/telemetrydata.go
+++ pkg/telemetry/telemetrydata/telemetrydata.go
@@ -15,10 +15,26 @@ type TEventProps map[string]interface{}
 
 // Exported constants for use in other packages
 const (
-	ConnType         = "conn:type"
-	ConnWshErrorCode = "conn:wsherrorcode"
-	BlockView        = "block:view"
-	BlockController  = "block:controller"
-	AiBackendType    = "ai:backendtype"
-	AiLocal          = "ai:local"
+	ConnType                   = "conn:type"
+	ConnWshErrorCode           = "conn:wsherrorcode"
+	BlockView                  = "block:view"
+	BlockController            = "block:controller"
+	AiBackendType              = "ai:backendtype"
+	AiLocal                    = "ai:local"
+	WaveAIAPIType              = "waveai:apitype"
+	WaveAIModel                = "waveai:model"
+	WaveAIChatId               = "waveai:chatid"
+	WaveAIStepNum              = "waveai:stepnum"
+	WaveAIInputTokens          = "waveai:inputtokens"
+	WaveAIOutputTokens         = "waveai:outputtokens"
+	WaveAINativeWebSearchCount = "waveai:nativewebsearchcount"
+	WaveAIRequestCount         = "waveai:requestcount"
+	WaveAIToolUseCount         = "waveai:toolusecount"
 )
+
+func MakeTEvent(eventType string) *TEvent {
+	return &TEvent{
+		EventType: eventType,
+		Props:     make(TEventProps),
+	}
+}
